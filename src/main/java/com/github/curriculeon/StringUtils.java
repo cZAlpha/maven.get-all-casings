eb525a894ecb9ea8bfaa735c3ce4fee7031ff316
Noah <nbklaus21@students.desu.edu>
2023-03-28 17:13:32 -0400

    Did a lil somethin

--- src/main/java/com/github/curriculeon/StringUtils.java
+++ src/main/java/com/github/curriculeon/StringUtils.java
@@ -1,4 +1,6 @@
 package com.github.curriculeon;
+import java.util.Arrays;
+import java.util.List;
 
 public class StringUtils {
     /**
@@ -7,7 +9,17 @@ public class StringUtils {
      * @return near-identical string whose characters at specified indices are capitalized
      */
     public static String upperCaseIndices(String string, Integer... indices) {
-        return null;
+        if (string == null || indices == null) {
+            throw new IllegalArgumentException("One or more of your inputted arguments is null.");
+        }
+        StringBuilder stringBuilder = new StringBuilder(string);
+        for (int index : indices) {
+            if (index < 0 || index >= stringBuilder.length()) {
+                throw new IllegalArgumentException("Index out of range.");
+            }
+            stringBuilder.setCharAt(index, Character.toUpperCase(stringBuilder.charAt(index)));
+        }
+        return stringBuilder.toString();
     }
 
 
@@ -18,7 +30,15 @@ public class StringUtils {
      * @return near-identical string with `valueToBeInserted` inserted at `index`
      */
     public static String insertAtIndex(String stringToBeManipulated, String valueToBeInserted, Integer index) {
-        return null;
+        if (stringToBeManipulated == null || valueToBeInserted == null || index == null) {
+            throw new IllegalArgumentException("One of the values inputted to the method's argument(s) was null.");
+        }
+        if (index < 0 || index > stringToBeManipulated.length()) {
+            throw new IllegalArgumentException("One of the values inputted to the method's argument(s) was not valid.");
+        }
+        StringBuilder stringBuilder = new StringBuilder(stringToBeManipulated);
+        stringBuilder.insert(index, valueToBeInserted);
+        return stringBuilder.toString();
     }
 
     /**
@@ -28,6 +48,14 @@ public class StringUtils {
      * @return near-identical string with character at `index` replaced with `replacementValue`
      */
     public static String replaceAtIndex(String stringToBeManipulated, Character replacementValue, Integer index) {
-        return null;
+        if (stringToBeManipulated == null || replacementValue == null || index == null) {
+            throw new IllegalArgumentException("One of the values inputted to the method's argument(s) was null.");
+        }
+        if (index < 0 || index > stringToBeManipulated.length()) {
+            throw new IllegalArgumentException("One of the values inputted to the method's argument(s) was not valid.");
+        }
+        StringBuilder stringBuilder = new StringBuilder(stringToBeManipulated);
+        stringBuilder.setCharAt(index, replacementValue);
+        return stringBuilder.toString();
     }
 }
